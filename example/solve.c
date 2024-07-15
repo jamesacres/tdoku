475bbfbc95d03fc947d952a73c5b07b9f8ed5982
James Acres <jamesacres@gmail.com>
2024-07-15 23:53:14 +0100

    Feature/wasm (#2)
    * Initial commit of wasm
    
    * undo unneccessary conversion for wasm
    
    * refactor options and update README with WASM
    
    * add broken generate example
    
    * adjust solve example to be minimal and accept input

--- example/solve.c
+++ example/solve.c
@@ -2,16 +2,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
-int main(int argc, const char **argv) {
-    size_t limit = argc > 1 ? atoll(argv[1]) : 10000;
-
+int main(int argc, char **argv) {
     char *puzzle = "2.6.3......1.65.7..471.8.5.5......29..8.194.6...42...1....428..6.93....5.7.....13";
+    int c;
+    while ((c = getopt (argc, argv, "p:")) != -1) {
+        switch (c) {
+            case 'p': {
+                puzzle = optarg;
+                break;
+            }
+            default: {
+              abort ();
+            }
+        }
+    }
+
     char solution[81];
     size_t size, guesses;
 
-    solution[0] = '\0';
-    size_t count = SolveSudoku(puzzle, limit, 0, solution, &guesses);
-    SolveSudoku(puzzle, 1, 0, solution, &guesses);
-    printf("%.81s:%ld:%.81s\n", puzzle, count, solution);
+    size_t count = SolveSudoku(puzzle, 2, 0, solution, &guesses);
+    if (count == 1) {
+      SolveSudoku(puzzle, 1, 0, solution, &guesses);
+      printf("%.81s\n", solution);
+    }
 }
