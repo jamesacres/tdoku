3d00178c38f86a4bee9c40bd8eefa88e6ce23ffc
James Acres <jamesacres@gmail.com>
2024-07-16 22:19:57 +0100

    export solve function

--- compile-wasm.sh
+++ compile-wasm.sh
@@ -1,1 +1,1 @@
-emcc example/solve.c build/libtdoku_static.a -O3 -o solve -lstdc++ -lm -o example/solve.js
+emcc example/solve.c build/libtdoku_static.a -O3 -o solve -lstdc++ -lm -o example/solve.js -sEXPORTED_FUNCTIONS=_solve -sEXPORTED_RUNTIME_METHODS=ccall
--- example/solve.c
+++ example/solve.c
@@ -4,27 +4,23 @@
 #include <string.h>
 #include <unistd.h>
 
-int main(int argc, char **argv) {
-    char *puzzle = "2.6.3......1.65.7..471.8.5.5......29..8.194.6...42...1....428..6.93....5.7.....13";
-    int c;
-    while ((c = getopt (argc, argv, "p:")) != -1) {
-        switch (c) {
-            case 'p': {
-                puzzle = optarg;
-                break;
-            }
-            default: {
-              abort ();
-            }
-        }
-    }
+#ifdef __cplusplus
+extern "C" {
+#endif
 
+
+char* solve(char *puzzle) {
+    char *result;
     char solution[81];
     size_t size, guesses;
-
     size_t count = SolveSudoku(puzzle, 2, 0, solution, &guesses);
     if (count == 1) {
       SolveSudoku(puzzle, 1, 0, solution, &guesses);
-      printf("%.81s\n", solution);
+      sprintf(result, "%.81s\n", solution);
     }
+    return result;
 }
+
+#ifdef __cplusplus
+}
+#endif
